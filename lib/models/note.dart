6df507e9913eca2bf1ea1956eae0d3bb846b122e
lucaronz88 <lucaronz88@gmail.com>
2025-04-07 20:27:11 +0700

    added model for note

---
+++ lib/models/note.dart
@@ -0,0 +1,36 @@
+class Note {
+  final String id;
+  final String title;
+  final String content;
+  final String userId;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.userId,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      userId: json['userId'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'content': content,
+      'user_id': userId,
+    };
+  }
+}
